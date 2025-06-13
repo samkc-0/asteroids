586b76ff4edb8b7ed3f460fa896ba8fdf667e213
samkc-0 <sam.k.c.0@icloud.com>
2025-06-13 10:40:01 +0100

    add circle class

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ import constants as k
 
 def main():
     pygame.init()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    screen = pygame.display.set_mode((k.Screen.WIDTH, k.Screen.HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt: float = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+        screen.fill("black")
+        clock.tick()
 
 if __name__ == "__main__":
     main()
